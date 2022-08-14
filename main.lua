7012840824576d4b9464a9d76aac26f848829a56
melvincurinier <melvin.curinier@etu.univ-lyon1.fr>
2022-08-14 21:33:15 +0100

    setup music

--- main.lua
+++ main.lua
@@ -84,6 +84,9 @@ function love.load()
         highScores = loadHighScores()
     })
 
+    gSounds['music']:play()
+    gSounds['music']:setLooping(true)
+
     love.keyboard.keysPressed = {}
 end
 
